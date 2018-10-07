d74dcc915ebcd2b967dfcf2fec1e708ca417527e
Itay Saadia <itay.1234567891000@gmail.com>
2018-10-07 23:01:15 +0300

    udated setup file

--- setup.sh
+++ setup.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 
 # VIM
-if [ -f "$USER/.vimrc" ]; then
+if [ -f "$HOME/.vimrc" ]; then
     mv $HOME/.vimrc $HOME/.vimrc.old  # creating backup of the last vimrc
     cp vim/vimrc $HOME/.vimrc
-    cp vim/vim_runtime $HOME/.vim_runtime
+     cp vim/vim_runtime $HOME/.vim_runtime
 fi
 
 # i3
-if [ -d "$USER/.i3" ]; then  # for manjaro-i3 (community version of manjaro)
-    mv $USER/.i3/config $USER/.i3/config.old
-    cp i3/config $USER/.i3/config
+if [ -d "$HOME/.i3/" ]; then  # for manjaro-i3 (community version of manjaro)
+    echo "backuping you old i3 config..."
+    mv $HOME/.i3/config $HOME/.i3/config.old
+    echo "your old i3 config in the path "
+    echo $HOME/.i3/config.old
+    echo "finishing your new i3config file!"
+    cp i3/config $HOME/.i3/config
+    echo "i3 configuration done."
 fi
-if [ -d $USER/.config/i3 ]; then
-    mv $USER/.config/i3/config $USER/.config/i3/config.old
-    cp i3/config $USER/.config/i3/config
+if [ -d $HOME/.config/i3/ ]; then
+    mv $HOME/.config/i3/config $HOME/.config/i3/config.old
+    cp i3/config $HOME/.config/i3/config
 fi
-
